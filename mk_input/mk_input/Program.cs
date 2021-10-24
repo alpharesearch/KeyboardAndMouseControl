025e1cffe11ab01af5620be69c5ae688748311f1
Markus Schulz <schulz@alpharesearch.de>
2021-10-24 13:56:04 -0400

    licensens stuff

--- mk_input/mk_input/Program.cs
+++ mk_input/mk_input/Program.cs
@@ -1,10 +1,19 @@
 ﻿/*
- * Created by SharpDevelop.
- * User: markus
- * Date: 10/13/2021
- * Time: 8:47 PM
- * 
- * To change this template use Tools | Options | Coding | Edit Standard Headers.
+    Keyboard Video Mouse control
+    Copyright (C) 2021  Markus Schulz
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 using System;
 using System.Runtime.InteropServices;
